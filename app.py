0128955f5c860ed0f51f315d33619e4642c8e051
jinsemengxiang <1025464584@qq.com>
2023-07-23 17:53:21 +0800

    后端项目初始化

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from flask.json import jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+# 如果是GET请求，直接下面这样写，默认methods=['GET']
+@app.route('/test')
+def test():
+    # 这种就是在url后面添加的参数，例如：http://localhost:127.0.0.1/test?q=123456,那么query的值就是123456
+    # 可以接收多个参数，例如：http://localhost:127.0.0.1/test?username=tcw&password=123456,然后用request.args.get('username')和request.args.get('password')获取
+    username = request.args.get('username')
+    password = request.args.get('password')
+    # 获取到数据后可以做逻辑判断，比如将username和password与数据库当中的做比对，然后正确的话，返回true，然后前端得到true就让他成狗跳转界面，错误的话返回false，前端让他重新输入
+    print(username + '===' + password)
+    data = {'message': '你好'}
+    return jsonify(data)
+
+
+# 如果是POST请求，并且传入的数据是JSON形式，按下面这样写
+@app.route('/postTest', methods=['POST'])
+def post_test():
+    json = request.json
+    username = json['username']
+    password = json['password']
+    print(username + '===' + password)
+    data = {'message': '你好'}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run()
